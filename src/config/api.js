800b004984cb6a8e0e142fa017e1e2c6dd28d020
elmurodvokhidov <elmurodvkh@gmail.com>
2024-06-16 21:27:11 +0500

    server api o'zgardi

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({ baseURL: "https://hqs-backend.onrender.com/api" });
 export default api;
